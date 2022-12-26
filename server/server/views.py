ce9d6f0ae92e647feba157c240210a736ad4f676
Michael Boyles <MichaelWBoyles@yahoo.com>
2022-12-26 12:23:30 -0700

    added axios and tested whoami

--- server/server/views.py
+++ server/server/views.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
 
 def index(request):
     print('index')
     homepage = open('static/index.html').read()
     return HttpResponse(homepage)
-    
+
+@api_view(['GET'])
+def whoami(request):
+    print("whoami")
+    user = {"name": "me", "email": "you@yahoo.com"}
+    return JsonResponse(data = user)
